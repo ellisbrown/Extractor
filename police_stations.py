f118a8284acf3886e53588c9eb68a20a7745b9b7
Ellis Brown <ellis.l.brown@vanderbilt.edu>
2017-02-19 15:40:58 -0600

    updated to allow resuming from partial progress due to an error

--- police_stations.py
+++ police_stations.py
@@ -5,7 +5,7 @@ from extract import Extractor
 
 BASE = "http://www.mapdevelopers.com/what-county-am-i-in.php?address="
 
-with open("stations.csv", 'r') as f:
+with open("stations_new.csv", 'r') as f:
     station_raw = list(csv.reader(f, delimiter=','))
 
     headers = station_raw[0]
@@ -19,17 +19,89 @@ ids = {
     'longitude': 'display_lng'
 }
 
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+
+
 extractor = Extractor()
 with extractor:
     for row in station_data:
-        station = row[2]
+        # state = states[row[1]]
+        station = row[2] # + " " + state
         url = BASE + station.replace(' ', '%20')
-        extractor.get(url)
-        idx = 3
-        for key, value in ids.items():
-            row[idx] = extractor.id_text(value)
-            idx += 1
+        if row[3] != '': 
+            # skip if data already loaded
+            print('PASS : ', row[0:3])
+        else:
+            try:
+                extractor.get(url)
+                idx = 3
+                for key, value in ids.items():
+                    row[idx] = extractor.id_text(value)
+                    idx += 1
+                print('SUCC : ', row[0:3])
+            except:
+                print('ERR  : ',row[0:3])
+                break 
 
 with open('stations_new.csv', 'wb') as f:
     np.savetxt(f, station_data, fmt='%s,%s,%s,%s,%s,%s,%s,%s', delimiter=",",
                header=(",".join(headers)), comments='')
+
